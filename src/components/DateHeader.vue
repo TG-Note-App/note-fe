a9182a65fc47b247bb354d95ed67ac0927111c9f
Alex Zhuravlev <leftemail17@mail.ru>
2025-02-19 15:52:03 +0300

    fix: delete sticky date header

--- src/components/DateHeader.vue
+++ src/components/DateHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <h3 class="text-xs sm:text-sm font-medium uppercase tracking-wider pl-2 
-    flex items-center group sticky top-4 z-10 py-2 text-gray-400">
+    flex items-center group top-4 z-10 py-2 text-gray-400">
     <span v-if="title === 'ЗАКРЕПЛЕНО'" class="w-3 h-3 mr-2 flex items-center">
       <i class="bi bi-pin text-xs sm:text-sm text-blue-400"></i>
     </span>
